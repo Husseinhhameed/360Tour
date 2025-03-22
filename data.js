ebc87623b0cfd6229fdc54dc8317087c5f91d93d
Husseinhhameed <hossein.h.hameed@gmail.com>
2025-03-22 23:42:10 +0300

    update
    update

---
+++ data.js
@@ -0,0 +1,183 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-54367367531_47db750168_6k",
+      "name": "54367367531_47db750168_6k",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": 2.867405915392334,
+        "pitch": 0.0127453433153768,
+        "fov": 1.2038499345169202
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.052265737158999,
+          "pitch": -0.11525332154283596,
+          "rotation": 1.5707963267948966,
+          "target": "1-54393469832_b4689d6070_6k"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.7657626931006742,
+          "pitch": -0.2504532294499704,
+          "title": "Daasdorf ",
+          "text": "Daasdorf am Berge is a small, peaceful village in the hills of Thuringia.<br data-start=\"190\" data-end=\"193\">\nYou can enjoy the fresh air and green fields all around.<br data-start=\"249\" data-end=\"252\">\nIt’s close to the famous city of Weimar, full of stories and history!<br data-start=\"321\" data-end=\"324\">\nKids can explore nature, see cute farm animals, and play outside.<br data-start=\"389\" data-end=\"392\">\nIt’s a quiet place where fun and learning go hand in hand!"
+        }
+      ]
+    },
+    {
+      "id": "1-54393469832_b4689d6070_6k",
+      "name": "54393469832_b4689d6070_6k",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.902972114005159,
+          "pitch": -8.723546329747478e-10,
+          "rotation": 1.5707963267948966,
+          "target": "2-54231650240_eff404aa25_k"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.3223810591544378,
+          "pitch": -0.17886687266556578,
+          "title": "<span style=\"font-size: 13px; background-color: rgba(58, 68, 84, 0.8);\">Cagliari&nbsp;</span>",
+          "text": "Cagliari is a sunny city by the blue sea on the island of Sardinia.<br data-start=\"165\" data-end=\"168\">\nYou can spot flamingos in the lagoons and climb up ancient castles!<br data-start=\"235\" data-end=\"238\">\nThe beaches are sandy and perfect for building big sandcastles.<br data-start=\"301\" data-end=\"304\">\nThere are cool caves and colorful streets to explore.<br data-start=\"357\" data-end=\"360\">\nCagliari is full of fun, nature, and tasty gelato too!"
+        }
+      ]
+    },
+    {
+      "id": "2-54231650240_eff404aa25_k",
+      "name": "54231650240_eff404aa25_k",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.439545655061492,
+          "pitch": -0.029313004717041125,
+          "rotation": 1.5707963267948966,
+          "target": "3-54399653367_c7e6f829f4_6k"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.2158939633898296,
+          "pitch": -0.06774715463047976,
+          "title": "<span style=\"font-size: 13px; background-color: rgba(58, 68, 84, 0.8);\">Molde&nbsp;</span>",
+          "text": "Molde is a lovely town surrounded by mountains and sparkling fjords.<br data-start=\"167\" data-end=\"170\">\nIn spring and summer, pretty flowers bloom everywhere!<br data-start=\"224\" data-end=\"227\">\nYou can take a boat ride or look for trolls in the forest trails.<br data-start=\"292\" data-end=\"295\">\nSnowy winters make it great for sledding and cozy fun.<br data-start=\"349\" data-end=\"352\">\nMolde is a magical place where nature feels like a fairy tale!"
+        }
+      ]
+    },
+    {
+      "id": "3-54399653367_c7e6f829f4_6k",
+      "name": "54399653367_c7e6f829f4_6k",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.652961998598119,
+          "pitch": 0.18979628852216024,
+          "rotation": 4.71238898038469,
+          "target": "3-54399653367_c7e6f829f4_6k"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.0170869703852325,
+          "pitch": -0.08703032712998393,
+          "title": "<span style=\"font-size: 13px; background-color: rgba(58, 68, 84, 0.8);\">Nuremberg&nbsp;</span>",
+          "text": "Nuremberg is a city full of castles, towers, and tasty gingerbread!<br data-start=\"164\" data-end=\"167\">\nYou can visit a toy museum and see where old kings once lived.<br data-start=\"229\" data-end=\"232\">\nThe streets look like fairy tales with colorful houses and cobblestones.<br data-start=\"304\" data-end=\"307\">\nIn winter, the Christmas market sparkles with lights and treats.<br data-start=\"371\" data-end=\"374\">\nNuremberg is a mix of fun history, cool stories, and yummy food!"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
